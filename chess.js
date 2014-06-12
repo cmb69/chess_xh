55f358419464a30b106af2ffb3d2bb149a765ad0
cmb69 <cmbecker69@gmx.de>
2014-06-12 10:21:29 +0000

    refactored JavaScript

--- chess.js
+++ chess.js
@@ -1,4 +1,4 @@
-if (typeof window.addEventListener === "function") {
+if (window.addEventListener) {
     window.addEventListener("load", function () {
         "use strict";
 
@@ -10,59 +10,78 @@ if (typeof window.addEventListener === "function") {
         }
 
         function getParams(button) {
-            var form = button.form,
-                params = {
-                    selected: form.selected.value,
-                    chess_game: form.chess_game.value,
-                    chess_ply: form.chess_ply.value,
-                    chess_flipped: form.chess_flipped.value,
-                    chess_action: button.value,
-                    chess_ajax: 1
-                };
-            return serialize(params);
+            var form = button.form;
+            return serialize({
+                selected: form.selected.value,
+                chess_game: form.chess_game.value,
+                chess_ply: form.chess_ply.value,
+                chess_flipped: form.chess_flipped.value,
+                chess_action: button.value,
+                chess_ajax: 1
+            });
         }
 
-
         function onSubmit(event) {
+            var target, form, request, method;
+
             function onSuccess(form, html) {
                 var container = form.parentNode.parentNode;
                 container.innerHTML = html;
                 form = container.querySelector(".chess_control_panel");
                 form.addEventListener("click", onSubmit);
             }
-            var target = event.target, form, request, method, url, body;
+
+            function onReadyStateChange() {
+                if (request.readyState === 4) {
+                    if (request.status === 200) {
+                        onSuccess(form, request.responseText);
+                    } else {
+                        form.removeEventListener("click", onSubmit);
+                    }
+                }
+            }
+
+            function getUrl() {
+                var result = location.pathname;
+                if (method === "GET") {
+                    result += "?" + getParams(target);
+                }
+                return result;
+            }
+
+            function getBody() {
+                var result = null;
+                if (method !== "GET") {
+                    result = getParams(target);
+                }
+                return result;
+            }
+
+            target = event.target;
             if (target.nodeName === "BUTTON") {
                 form = target.form;
                 method = form.method.toUpperCase();
-                url = location.pathname;
-                if (method === "GET") {
-                    url += "?" + getParams(target);
-                } else {
-                    body = getParams(target);
-                }
                 request = new XMLHttpRequest();
-                request.open(method, url);
+                request.open(method, getUrl());
                 if (method === "POST") {
                     request.setRequestHeader("Content-Type",
                             "application/x-www-form-urlencoded");
                 }
-                request.onreadystatechange = function () {
-                    if (request.readyState === 4) {
-                        if (request.status === 200) {
-                            onSuccess(form, request.responseText);
-                        } else {
-                            form.removeEventListener("click", onSubmit);
-                        }
-                    }
-                };
-                request.send(body);
+                request.onreadystatechange = onReadyStateChange;
+                request.send(getBody());
                 event.preventDefault();
             }
         }
 
-        var forms = document.getElementsByClassName("chess_control_panel");
-        Array.prototype.forEach.call(forms, function (form) {
-            form.addEventListener("click", onSubmit);
-        });
+        function registerClickHandlers() {
+            var forms = document.getElementsByClassName("chess_control_panel");
+            Array.prototype.forEach.call(forms, function (form) {
+                form.addEventListener("click", onSubmit);
+            });
+
+        }
+
+        registerClickHandlers();
+
     });
 }
