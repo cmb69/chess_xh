91d338ef2919a1b6dcc34cdfa10d63b09d7de922
cmb69 <cmbecker69@gmx.de>
2014-06-13 13:28:06 +0000

    added German localization and manual

---
+++ languages/de.php
@@ -0,0 +1,18 @@
+<?php
+
+$plugin_tx['chess']['menu_main']="Import";
+
+$plugin_tx['chess']['label_flip']="Drehen";
+$plugin_tx['chess']['label_start']="Start";
+$plugin_tx['chess']['label_previous']="Zurück";
+$plugin_tx['chess']['label_goto']="Gehe zu";
+$plugin_tx['chess']['label_next']="Weiter";
+$plugin_tx['chess']['label_end']="Ende";
+$plugin_tx['chess']['label_import']="Import";
+
+$plugin_tx['chess']['message_invalid_name']="Der Name \"%s\" ist ungültig!";
+$plugin_tx['chess']['message_load_error']="Die Schachdatei \"%s\" kann nicht geladen werden!";
+
+$plugin_tx['chess']['alt_icon']="Springer auf Schachbrettfragment";
+
+?>
