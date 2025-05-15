6cf36a210115b74e262808b9dcc20daef0b02fb3
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-15 18:48:32 +0200

    Test Dic

--- tests/unit/DicTest.php
+++ tests/unit/DicTest.php
@@ -2,8 +2,26 @@
 
 namespace Chess;
 
+/** @small */
 class DicTest extends TestCase
 {
+    public function setUp(): void
+    {
+        global $pth, $plugin_tx;
+        $pth = ["folder" => ["plugins" => ""]];
+        $plugin_tx = ["chess" => []];
+    }
+
+    public function testMakesChessController(): void
+    {
+        $this->assertInstanceOf(ChessController::class, Dic::chessController());
+    }
+
+    public function testMakesImportCommand(): void
+    {
+        $this->assertInstanceOf(ImportCommand::class, Dic::importCommand());
+    }
+
     public function testMakesInfoView(): void
     {
         $this->assertInstanceOf(InfoView::class, Dic::infoView());
