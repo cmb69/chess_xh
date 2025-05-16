744d1d931372976f7cdcd4eb5e0d927c54b49c37
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-16 17:12:37 +0200

    Extract InfoViewTest::sut()

--- tests/InfoViewTest.php
+++ tests/InfoViewTest.php
@@ -13,15 +13,23 @@ class InfoViewTest extends TestCase
     /** @var DocumentStore */
     private $store;
 
+    /** @var View */
+    private $view;
+
     public function setUp(): void
     {
         $this->store = new DocumentStore("../../content/chess/");
+        $this->view = new View("./views/", XH_includeVar("./languages/en.php", "plugin_tx")["chess"]);
+    }
+
+    private function sut(): InfoView
+    {
+        return new InfoView("./", $this->store, new FakeSystemChecker(), $this->view);
     }
 
     public function testRendersInfo(): void
     {
-        $view = new View("./views/", XH_includeVar("./languages/en.php", "plugin_tx")["chess"]);
-        $response = (new InfoView("./", $this->store, new FakeSystemChecker(), $view))->render();
+        $response = $this->sut()->render();
         Approvals::verifyHtml($response);
     }
 }
